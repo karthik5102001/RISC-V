adf5fe1e890e6ee20135648db12809202e9098a8
Karthik S <116733736+karthik5102001@users.noreply.github.com>
2025-07-23 11:07:24 +0530

    Uploading RTL and Testbench Files
    This is just a basic run on PICORV a 32 bit processor with testbench where we dump the hex files, which further taken by the memory inside PICORV processor.
    Still the structure of the processor needed to be explored.....
    The C program will take the value from its interface to the assembly language source file and generate the hex file through RISCV GCC complier

--- Scripts/Run/Run.sh
+++ Scripts/Run/Run.sh
@@ -1,7 +1,7 @@
-riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32im -o Test_1.o Test_1.c 
-riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32im -o load.o load.S
+riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32im -o ../../Cfiles/TEST_1/Test_1.o ../../Cfiles/TEST_1/Test_1.c 
+riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32im -o ../../Cfiles/TEST_1/load.o ../../Cfiles/TEST_1/load.S
 
-riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32im -o syscalls.o syscalls.c
+riscv64-unknown-elf-gcc -c -mabi=ilp32 -march=rv32im -o ../../Cfiles/TEST_1/syscalls.o ../../Cfiles/TEST_1/syscalls.c
 riscv64-unknown-elf-gcc -mabi=ilp32 -march=rv32im -Wl,--gc-sections -o firmware.elf load.o Test_1.o syscalls.o -T riscv.ld -lstdc++
 chmod -x firmware.elf
 riscv64-unknown-elf-gcc -mabi=ilp32 -march=rv32im -nostdlib -o start.elf start.S -T start.ld -lstdc++
@@ -11,6 +11,6 @@ riscv64-unknown-elf-objcopy -O verilog firmware.elf firmware.tmp
 cat start.tmp firmware.tmp > firmware.hex
 python3 hex8tohex32.py firmware.hex > firmware32.hex
 rm -f start.tmp firmware.tmp
-iverilog -o testbench.vvp testbench.v picorv32.v
-chmod -x testbench.vvp
-vvp -N testbench.vvp
+iverilog -o ../../Cfiles/TEST_1/SIM/testbench.vvp ../../Cfiles/TEST_1/Testbench/Testbench_picorv32.v ../../Cfiles/TEST_1/RTL/picorv32.v
+chmod -x ../../Cfiles/TEST_1/SIM/testbench.vvp
+vvp -N ../../Cfiles/TEST_1/SIM/testbench.vvp
